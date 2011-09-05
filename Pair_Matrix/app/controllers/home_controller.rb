91b04a4f14e7e360872a82f2eed183f0a7db5468
Vinay Kaimal <vinaykaimal44@gmail.com>
2011-09-06 00:09:04 +0530

    first commit

---
+++ Pair_Matrix/app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  @names=["Vinay", "Ash", "Piyush"]
+  @numbers_1=["1","2","1"]
+  @numbers_2=["2","1"," "]
+  @numbers_3=["2",""," "]
+  @n = "vinay"
+  def index
+
+  end
+end
---
+++ Pair_Matrix/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PairMatrix::Application.config.secret_token = '017f57de6003c1fa9e22542a02c1e9b71e446877dbb42806632f5f19a4fc92cba4d89ca44e8193754e334aeea0855b176b25cb224dc02f4d7079f9459d2e8848'
